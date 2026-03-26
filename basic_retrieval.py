35f4693dd7e10357e2b6a893c1033876e0f461cf
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2026-03-25 22:33:34 -0400

    basic retrieval

---
+++ basic_retrieval.py
@@ -0,0 +1,10 @@
+import enable_langfuse_tracing  # noqa: F401
+from smolagents import CodeAgent, WebSearchTool, InferenceClientModel
+
+search_tool = WebSearchTool()
+model = InferenceClientModel()
+
+agent = CodeAgent(model=model, tools=[search_tool])
+
+response = agent.run("Search for luxury superhero-themed party ideas, including decorations, entertainment, and catering.")
+print(response)
